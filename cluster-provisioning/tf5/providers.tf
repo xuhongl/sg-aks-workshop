9bf143084b85336c8dfd82668609952964ddb015
Xuhong Liu <xuhliu@microsoft.com>
2020-06-28 14:38:47 -0400

    lufx

--- cluster-provisioning/tf5/providers.tf
+++ cluster-provisioning/tf5/providers.tf
@@ -9,14 +9,6 @@ provider "github" {
   version      = "=2.4.1"
 }
 
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.demo.kube_admin_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.demo.kube_admin_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.demo.kube_admin_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.demo.kube_admin_config.0.cluster_ca_certificate)
-  #version                = "=0.6.0"
-  version = "=1.11.1"
-}
 
 provider "tls" {
   version = "=2.1"
